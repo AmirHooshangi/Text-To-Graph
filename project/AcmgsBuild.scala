c63b174ad3b0822b285a3ea516d68aa59d6930b6
Amir Hooshangi <amirhoshangi@gmail.com>
2014-10-31 23:37:56 +0330

    AcmgsBuild.scala: openlp and ws4j added to the dependencies

--- project/AcmgsBuild.scala
+++ project/AcmgsBuild.scala
@@ -11,8 +11,13 @@ object AcmgsBuild extends Build {
       organization := "com.velorin",
       version := "0.1-SNAPSHOT",
       scalaVersion := "2.11.2",
+      resolvers += "Local Maven Repository" at "file://"+Path.userHome.absolutePath+"/.m2/repository",
       resolvers += "Typesafe Releases" at "http://repo.typesafe.com/typesafe/releases",
-      libraryDependencies += "com.typesafe.akka" % "akka-actor_2.10" % "2.3.6"
+      libraryDependencies ++= Seq(
+        "com.typesafe.akka" % "akka-actor_2.10" % "2.3.6",
+        "org.apache.opennlp" % "opennlp-tools" % "1.5.3",
+        "edu.cmu.lti" % "ws4j" % "1.0.1"
+      )
     )
   )
 }
