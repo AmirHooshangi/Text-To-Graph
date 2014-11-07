ec20100d938ce7605c447ad4044fcb8b8cf9c5ba
Amir Hooshangi <amirhoshangi@gmail.com>
2014-11-07 14:10:49 +0330

    HtmlToTextActor: apache tika added to extract the text from Html and then sends new URLEvent to the next actor

--- src/main/scala/com/velorin/actors/HtmlToTextActor.scala
+++ src/main/scala/com/velorin/actors/HtmlToTextActor.scala
@@ -1,15 +1,33 @@
 package com.velorin.actors
 
 import akka.actor.Actor
+import akka.actor.ActorRef
+import java.io.BufferedReader
+import java.io.InputStream
+import java.io.InputStreamReader
+import java.net.URL
+import org.apache.tika.metadata.Metadata
+import org.apache.tika.parser.ParseContext
+import org.apache.tika.parser.html.HtmlParser
+import org.apache.tika.sax.BodyContentHandler
+import org.xml.sax.ContentHandler
 
-class HtmlToTextActor extends Actor {
+
+class HtmlToTextActor(actorRef: ActorRef) extends Actor {
 
   def receive =  {
-
-    case test: String =>
-      {
-        Thread.sleep(2000)
-        println("Helloo world")
+    case URLEvent(url: URL, text: String ) => {
+      println("message recieved i repeat ! message recieved")
+      actorRef ! new URLEvent(url, extractText(url))
       }
+  }
+
+  def extractText(url: URL) : String = {
+    val input:  InputStream =  url.openStream()
+    val handler: ContentHandler = new BodyContentHandler()
+    val metadata: Metadata  = new Metadata()
+    new HtmlParser().parse(input, handler, metadata, new ParseContext())
+    handler.toString()
+//    println(plainText)
   }
 }
