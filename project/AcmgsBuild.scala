b3f591f3dbff15a55cec74704b564a16f920926d
Amir Hooshangi <amirhoshangi@gmail.com>
2014-11-07 14:09:08 +0330

    project/AcmgsBuild.scala: apache tika added

--- project/AcmgsBuild.scala
+++ project/AcmgsBuild.scala
@@ -8,15 +8,18 @@ object AcmgsBuild extends Build {
     base = file("."),
     settings = Project.defaultSettings ++ Seq(
       name := "ACMG-S",
+      fork in run := true,
       organization := "com.velorin",
       version := "0.1-SNAPSHOT",
       scalaVersion := "2.11.2",
       resolvers += "Local Maven Repository" at "file://"+Path.userHome.absolutePath+"/.m2/repository",
       resolvers += "Typesafe Releases" at "http://repo.typesafe.com/typesafe/releases",
       libraryDependencies ++= Seq(
-        "com.typesafe.akka" % "akka-actor_2.10" % "2.3.6",
+        "com.typesafe.akka" %% "akka-actor" % "2.3.6",
         "org.apache.opennlp" % "opennlp-tools" % "1.5.3",
-        "edu.cmu.lti" % "ws4j" % "1.0.1"
+        "edu.cmu.lti" % "ws4j" % "1.0.1",
+        "org.apache.tika" % "tika-core" % "1.6",
+        "org.apache.tika" % "tika-parsers" % "1.6"
       )
     )
   )
