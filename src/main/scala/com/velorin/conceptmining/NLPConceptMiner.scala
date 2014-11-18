1bfea2d64a5d6368d6e6e4390bbfd8568d4d9775
Amir Hooshangi <amirhoshangi@gmail.com>
2014-11-18 12:06:42 +0330

    NLPConceptMiner: imperatvie functions deleted and class changed to object

--- src/main/scala/com/velorin/conceptmining/NLPConceptMiner.scala
+++ src/main/scala/com/velorin/conceptmining/NLPConceptMiner.scala
@@ -7,41 +7,18 @@ import opennlp.tools.parser.ParserFactory
 import opennlp.tools.parser.ParserModel
 import opennlp.tools.sentdetect.SentenceDetectorME
 import opennlp.tools.sentdetect.SentenceModel
-//import scala.collection.mutable.Set
 
-class NLPConceptMiner extends ConceptMiner {
+object NLPConceptMiner extends ConceptMiner {
 
-  val concepts = Set[String]()
+  val parserModel = new ParserModel(getClass.getResource("/en-parser-chunking.bin"));
+  val sentenceModel = new SentenceModel(getClass.getResource("/en-sent.bin"));
 
   def getListOfConcepts(text: String): Set[String] = {
     val parser = ParserFactory.create(NLPConceptMiner.parserModel)
     val listOfSentences = getListOfSentences(text)
     val parses: Array[Parse] = listOfSentences.flatMap(x => ParserTool.parseLine(x, parser, 1))
     val concepts = parses.flatMap(recursiveTypefinder)
-    println("Size of List is = " + concepts.toSet.size)
     concepts.toSet
-//    for (tokenizedSentence <- listOfSentences) {
-//      var parses: Array[Parse] = ParserTool.parseLine(tokenizedSentence, parser, 1)
-//      concepts ::: recursiveTypefinder(parses(0))
-//    }
-
-  }
-
-  def recursiveTypefinder1(parse: opennlp.tools.parser.Parse): Unit = {
-    /*
-     * TODO: CHECKING ONE OR TWO WORDS NP's
-     */
-
-    if (parse.getType().equals("NN") || parse.getType().equals("JJ")
-      || parse.getType().equals("NNS")
-      || parse.getType().equals("NNP")) {
-      println(parse.getCoveredText)
-//      concepts.add(parse.getCoveredText)
-    }
-
-    for (child <- parse.getChildren) {
-      recursiveTypefinder1(child)
-    }
   }
 
   def recursiveTypefinder(parse: opennlp.tools.parser.Parse): List[String] = {
@@ -56,10 +33,4 @@ class NLPConceptMiner extends ConceptMiner {
     val sentenceFinderME = new SentenceDetectorME(NLPConceptMiner.sentenceModel)
     sentenceFinderME.sentDetect(paragraph)
   }
-}
-
-object NLPConceptMiner {
-
-  val parserModel = new ParserModel(getClass.getResource("/en-parser-chunking.bin"));
-  val sentenceModel = new SentenceModel(getClass.getResource("/en-sent.bin"));
 }
