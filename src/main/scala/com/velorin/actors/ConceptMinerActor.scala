08ae37465a7e4bfe99413e02a0671784e5258b5c
Amir Hooshangi <amirhoshangi@gmail.com>
2014-12-09 21:30:10 +0330

    ConceptMinerActor: ActorRef added to constructor and URLEvent refactored (List added)

--- src/main/scala/com/velorin/actors/ConceptMinerActor.scala
+++ src/main/scala/com/velorin/actors/ConceptMinerActor.scala
@@ -6,12 +6,12 @@ import java.net.URL
 import com.velorin.conceptmining.ConceptMiner
 import com.velorin.conceptmining.NLPConceptMiner
 
-class ConceptMinerActor extends Actor{
+class ConceptMinerActor(actorRef: ActorRef) extends Actor{
 
  def receive =  {
-    case URLEvent(url: URL, text: String) => {
+    case URLEvent(url: URL, text: String, setOfConcepts: List[String]) => {
       NLPConceptMiner.getListOfConcepts(text).foreach(println)
+      actorRef ! URLEvent(url, "a", NLPConceptMiner.getListOfConcepts(text).toList)
       }
   }
-
 }
