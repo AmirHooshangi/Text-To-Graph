a73155f2df5e75e49629bd9816c33c6ad00ccf1c
Amir Hooshangi <amirhoshangi@gmail.com>
2014-12-16 14:16:13 +0330

    AcmgsBuild: scala version reduced to 2.10.4 because of mismatch with neo4j. Neo4j dependencies added to the file

--- project/AcmgsBuild.scala
+++ project/AcmgsBuild.scala
@@ -11,8 +11,10 @@ object AcmgsBuild extends Build {
       fork in run := true,
       organization := "com.velorin",
       version := "0.1-SNAPSHOT",
-      scalaVersion := "2.11.2",
+      scalaVersion := "2.10.4",
       resolvers += "Local Maven Repository" at "file://"+Path.userHome.absolutePath+"/.m2/repository",
+      resolvers +=  "sonatype-snapshots" at  "https://oss.sonatype.org/content/repositories/snapshots/",
+      resolvers += "neo4j-releases" at "http://m2.neo4j.org/releases/",
       resolvers += "Typesafe Releases" at "http://repo.typesafe.com/typesafe/releases",
       libraryDependencies ++= Seq(
         "com.typesafe.akka" %% "akka-actor" % "2.3.6",
@@ -20,6 +22,8 @@ object AcmgsBuild extends Build {
         "org.apache.opennlp" % "opennlp-tools" % "1.5.3",
         "edu.cmu.lti" % "ws4j" % "1.0.1",
         "edu.cmu.lti" % "jawjaw" % "1.0.2",
+        "org.neo4j" % "neo4j" % "1.9.5",
+        "eu.fakod" %% "neo4j-scala" % "0.3.0",
         "org.apache.tika" % "tika-core" % "1.6",
         "org.apache.tika" % "tika-parsers" % "1.6"
       )
