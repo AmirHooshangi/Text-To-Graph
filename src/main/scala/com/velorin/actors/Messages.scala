181d09db1049484075f881bd916251d1c56cebcd
Amir Hooshangi <amirhoshangi@gmail.com>
2014-12-16 14:21:08 +0330

    Messages: case class SaveToDBEvent added

--- src/main/scala/com/velorin/actors/Messages.scala
+++ src/main/scala/com/velorin/actors/Messages.scala
@@ -3,3 +3,6 @@ package com.velorin.actors
 import java.net.URL
 
 case class URLEvent(url: URL, text: String, setOfConcepts: List[String] )
+
+case class SaveToDBEvent(url: URL, concepts: List[List[String]] )
+
