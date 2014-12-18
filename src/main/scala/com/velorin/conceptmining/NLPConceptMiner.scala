0077efcd2613e2120428f86d2945b9473a95cfe4
Amir Hooshangi <amirhoshangi@gmail.com>
2014-12-18 15:50:41 +0330

    NLPConceptMiner: List of available POS tags added as a documentation to the recursiveTypefinder() function

--- src/main/scala/com/velorin/conceptmining/NLPConceptMiner.scala
+++ src/main/scala/com/velorin/conceptmining/NLPConceptMiner.scala
@@ -21,10 +21,52 @@ object NLPConceptMiner extends ConceptMiner {
     concepts.toSet
   }
 
-  //TODO: two words NN, removing 's and dots
 
+
+/*
+
+  These are the list of tags you can use:
+
+  CC Coordinating conjunction
+  CD Cardinal number
+  DT Determiner
+  EX Existential there
+  FW Foreign word
+  IN Preposition or subordinating conjunction
+  JJ Adjective
+  JJR Adjective, comparative
+  JJS Adjective, superlative
+  LS List item marker
+  MD Modal
+  NN Noun, singular or mass
+  NNS Noun, plural
+  NNP Proper noun, singular
+  NNPS Proper noun, plural
+  PDT Predeterminer
+  POS Possessive ending
+  PRP Personal pronoun
+  PRP$ Possessive pronoun
+  RB Adverb
+  RBR Adverb, comparative
+  RBS Adverb, superlative
+  RP Particle
+  SYM Symbol
+  TO to
+  UH Interjection
+  VB Verb, base form
+  VBD Verb, past tense
+  VBG Verb, gerund or present participle
+  VBN Verb, past participle
+  VBP Verb, non 3rd person singular present
+  VBZ Verb, 3rd person singular present
+  WDT Wh­determiner
+  WP Wh­pronoun
+  WP$ Possessive wh­pronoun
+  WRB Whadverb
+*/
   def recursiveTypefinder(parse: opennlp.tools.parser.Parse): List[String] = {
     val concepts = parse.getType match {
+      //TODO: two words NN, removing 's and dots
       case "NN" | "JJ" /* | "NNS" | "NNP" */  => { List(if(parse.getCoveredText.contains(".") |
         parse.getCoveredText.contains(",") | parse.getCoveredText.contains("’s") | parse.getCoveredText.contains("\"") )
         parse.getCoveredText.replaceAll("(\"|.|’s|'s|,)", "")
