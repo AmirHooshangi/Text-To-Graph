aad8d2dc4828fc7bc62efa37d405ba3b50b94037
Amir Hooshangi <amirhoshangi@gmail.com>
2014-12-20 16:04:12 +0330

    Messages: modified to use NodeList in it's signature

--- src/main/scala/com/velorin/actors/Messages.scala
+++ src/main/scala/com/velorin/actors/Messages.scala
@@ -2,7 +2,9 @@ package com.velorin.actors
 
 import java.net.URL
 
+import com.velorin.model.NodeList
+
 case class URLEvent(url: URL, text: String, setOfConcepts: List[String] )
 
-case class SaveToDBEvent(url: URL, concepts: List[List[String]] )
+case class SaveToDBEvent(url: URL, concepts: List[List[NodeList]] )
 
