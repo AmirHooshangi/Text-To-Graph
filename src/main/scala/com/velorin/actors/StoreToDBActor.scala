921cb6cf1173e53177ee5cd3d57ba49745735cea
Amir Hooshangi <amirhoshangi@gmail.com>
2014-12-20 16:08:08 +0330

    StoreToDBActor: modified to use NodeList's as a data structure for storing concepts to the Neo4j

--- src/main/scala/com/velorin/actors/StoreToDBActor.scala
+++ src/main/scala/com/velorin/actors/StoreToDBActor.scala
@@ -3,6 +3,7 @@ package com.velorin.actors
 import java.net.URL
 
 import akka.actor.Actor
+import com.velorin.model.{Node, NodeList}
 import eu.fakod.neo4jscala.{Cypher, EmbeddedGraphDatabaseServiceProvider, Neo4jWrapper}
 
 import scala.sys.ShutdownHookThread
@@ -32,10 +33,11 @@ class StoreToDBActor extends Actor with Neo4jWrapper with EmbeddedGraphDatabaseS
 
 
   def receive  = {
-    case SaveToDBEvent(url: URL, concepts: List[List[String]] ) => {
+    case SaveToDBEvent(url: URL, concepts: List[List[NodeList]]  ) => {
 
       for(i <- concepts)
-        storeToDB(i)
+        for(j <- i)
+        storeToDB(j)
 
       shutdown(ds)
 
@@ -43,25 +45,25 @@ class StoreToDBActor extends Actor with Neo4jWrapper with EmbeddedGraphDatabaseS
     }
   }
 
-  def storeToDB(concepts: List[String]) = {
+  def storeToDB(concepts: NodeList) = {
 
-    for(i <- concepts) {
+    for(i <- concepts.listOfNodes) {
       storePairs(concepts.head, i)
 
     }
   }
-
-  def storePairs(first:String, second: String) = {
-    if(!exists(first)) create_node(first, first)
-    if(!exists(second)) create_node(second, second)
-    createRelation(first, second)
+ 
+  def storePairs(head: Node, node2: Node) = {
+    if(!exists(head.name)) create_node(head.name, head.name)
+    if(!exists(node2.name)) create_node(node2.name, node2.name)
+    createRelation(head, node2)
   }
 
-  def createRelation(first:String, second: String) = {
+  def createRelation(node1: Node, node2: Node) = {
     val query = "START n=node(*), m=node(*)  " +
-      "\n where has(n.name) and has(m.name) and n.name = "+ "\'"+ first + "\'" +
-      "\n and m.name = " + "\'" +  second + "\'" +
-      "\n create (n)-[:R]->(m)"
+      "\n where has(n.name) and has(m.name) and n.name = "+ "\'"+ node1.name + "\'" +
+      "\n and m.name = " + "\'" +  node2.name + "\'" +
+      "\n create (n)-[r:R { weight : " + "\'" + node2.weight + "\'" +" }]-(m)"
     withTx {
       implicit neo =>
         query.execute
