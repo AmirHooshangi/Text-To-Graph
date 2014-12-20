a429c7f91df313743d4b6f147d73f34091c4497a
Amir Hooshangi <amirhoshangi@gmail.com>
2014-12-20 16:06:45 +0330

    RelationFinderActor: modified to add weight to Node class using WordNet API. relatedConcepts() method modified to avoid null and now uses Option[T]

--- src/main/scala/com/velorin/actors/RelationFinderActor.scala
+++ src/main/scala/com/velorin/actors/RelationFinderActor.scala
@@ -3,8 +3,7 @@ package com.velorin.actors
 import akka.actor.Actor
 import akka.actor.ActorRef
 import java.net.URL
-import com.velorin.conceptmining.ConceptMiner
-import com.velorin.conceptmining.NLPConceptMiner
+import com.velorin.model.{NodeList, Node}
 import com.velorin.wordnet.WsFactory
 
 class RelationFinderActor(actorRef: ActorRef) extends Actor{
@@ -16,26 +15,27 @@ class RelationFinderActor(actorRef: ActorRef) extends Actor{
 
                               relatedList = relatedConcepts(concepts(i), concepts.slice( i+1, concepts.size))
 
-      } yield  if(  relatedList != null) concepts(i) :: relatedList else relatedList
+      } yield  {relatedList match  { case Some(s) => relatedList.toList ; case None => List() }}
 
-      listOfLists.filter(_ != null).foreach(println)
-      actorRef ! SaveToDBEvent(url, listOfLists.filter(_ != null).toList)
+      println("it comes here")
+      listOfLists.filter(x => !x.isEmpty).foreach(println)
+      actorRef ! SaveToDBEvent(url, listOfLists.filter(x => !x.isEmpty).toList)
     }
-
   }
 
-  def relatedConcepts(arg1: String, arg2: List[String]) : List[String] = {
-
-    val list = for { i <- arg2; if areRelated(arg1,i)} yield i
+  def relatedConcepts(arg1: String, arg2: List[String]) : Option[NodeList] = {
+    
+    val list = for { i <- arg2; result = areRelated(arg1,i); weight = result._2  ; if result._1 } yield Node(i,i, weight)
     //TODO: null is avoided in Scala please consider it :)
-    if(list.isEmpty) null else list
+    if(list.isEmpty) None else Some(NodeList(Node(arg1,arg1,"0"), list))
   }
 
-  def areRelated(concept1: String, concept2: String): Boolean = {
+  def areRelated(concept1: String, concept2: String): (Boolean, String) = {
 
     val function =  WsFactory.calculationFunction("WuPalmer")
     println("similiarity bet " + concept1 + " and " + concept2 + " = " + function(concept1,concept2))
+    val weight = function(concept1,concept2)
     //TODO: 0.4 is a constant, it should be customizable
-    if(function(concept1,concept2) > 0.4 ) true else false
+    if( weight > 0.4 ) (true, weight.toString) else (false , weight.toString)
   }
 }
